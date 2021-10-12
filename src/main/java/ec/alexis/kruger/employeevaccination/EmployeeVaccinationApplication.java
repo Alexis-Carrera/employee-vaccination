a0143c5515bc9c1295da469a0bee627e29ad6501
Alexis Carrera <alexis.carrera@devsu.com>
2021-10-12 18:14:19 -0500

    first commit

---
+++ src/main/java/ec/alexis/kruger/employeevaccination/EmployeeVaccinationApplication.java
@@ -0,0 +1,13 @@
+package ec.alexis.kruger.employeevaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeVaccinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeVaccinationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/alexis/kruger/employeevaccination/EmployeeVaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.alexis.kruger.employeevaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeVaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
